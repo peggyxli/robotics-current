84f4c7436a274ff1040f22e61c5fa4162002fd9c
Peggy Li <xpeggyxli@gmail.com>
2017-11-06 17:19:59 -0500

    Update formatting for real-local.cc

--- real-local.cc
+++ real-local.cc
@@ -1,15 +1,8 @@
 /**
  * real-local.cc
- * 
- * Sample code for a robot that has two front bumpers and a laser scanner,
- * and. Suitable for use with the roomba.
- *
  * This version is intended to run with the AMCL localization proxy,
  * which provides multiple hypotheses.
- *
- * Written by: Simon Parsons
- * Date:       22nd November 2009
- *  
+ * 
  **/
 
 
@@ -17,79 +10,56 @@
 #include <libplayerc++/playerc++.h>
 using namespace PlayerCc;  
 
-/**
- * Function headers
- *
- **/
-
+//Function headers
 player_pose2d_t readPosition(LocalizeProxy& lp);
 void printLaserData(LaserProxy& sp);
 void printRobotData(BumperProxy& bp, player_pose2d_t pose);
 
-/**
- * main()
- *
- **/
+int main(int argc, char *argv[]) {  
 
-int main(int argc, char *argv[])
-{  
+	int counter = 0;
+	double speed;           
+	double turnrate;        
+	player_pose2d_t  pose;   // For handling localization data
+	player_laser_data laser; // For handling laser data
 
-  // Variables
-  int counter = 0;
-  double speed;            // How fast do we want the robot to go forwards?
-  double turnrate;         // How fast do we want the robot to turn?
-  player_pose2d_t  pose;   // For handling localization data
-  player_laser_data laser; // For handling laser data
+	// Set up proxies. These are the names we will use to connect to 
+	// the interface to the robot.
+	PlayerClient    robot("localhost");  
+	BumperProxy     bp(&robot,0);  
+	Position2dProxy pp(&robot,0);
+	LocalizeProxy   lp (&robot, 0);
+	LaserProxy      sp (&robot, 0);
 
-  // Set up proxies. These are the names we will use to connect to 
-  // the interface to the robot.
-  PlayerClient    robot("localhost");  
-  BumperProxy     bp(&robot,0);  
-  Position2dProxy pp(&robot,0);
-  LocalizeProxy   lp (&robot, 0);
-  LaserProxy      sp (&robot, 0);
+	pp.SetMotorEnable(true);
 
-  // Allow the program to take charge of the motors (take care now)
-  pp.SetMotorEnable(true);
-
-  // Main control loop
-  while(true) 
-    {    
-      // Update information from the robot.
-      robot.Read();
-      // Read new information about position
-      pose = readPosition(lp);
-      // Print information about the laser. Check the counter first to stop
-      // problems on startup
-      if(counter > 2){
-	printLaserData(sp);
-      }
-
-      // Print data on the robot to the terminal
-      printRobotData(bp, pose);
+	//Control loop
+	while(true) {    
       
-      // If either bumper is pressed, stop. Otherwise just go forwards
+    	robot.Read();				//Update information
+    	pose = readPosition(lp);	//Read position
 
-      if(bp[0] || bp[1]){
-	speed= 0;
-	turnrate= 0;
-      } 
-      else {
-	speed=.1;
-        turnrate = 0;
-      }     
+		if(counter > 2)	{			//Counter checks for errors on startup
+			printLaserData(sp);		//Prints laser data
+		}
+			
+      	printRobotData(bp, pose);	//Print data to the terminal
+      
+      	if(bp[0] || bp[1]){			//Stop if either bumper is pressed
+			speed= 0;
+			turnrate= 0;
+      	} else {					//Else go forward
+			speed=.1;
+        	turnrate = 0;
+      	}     
 
-      // What are we doing?
-      std::cout << "Speed: " << speed << std::endl;      
-      std::cout << "Turn rate: " << turnrate << std::endl << std::endl;
+      	std::cout << "Speed: " << speed << std::endl;      
+      	std::cout << "Turn rate: " << turnrate << std::endl << std::endl;
 
-      // Send the commands to the robot
-      pp.SetSpeed(speed, turnrate);  
-      // Count how many times we do this
-      counter++;
+      	pp.SetSpeed(speed, turnrate);  
+      	counter++;					//Increment number of loops
     }
-  
-} // end of main()
+}
 
 
 /**
@@ -106,87 +76,72 @@ int main(int argc, char *argv[])
  *
  **/
 
-player_pose2d_t readPosition(LocalizeProxy& lp)
-{
+player_pose2d_t readPosition(LocalizeProxy& lp) {
 
-  player_localize_hypoth_t hypothesis;
-  player_pose2d_t          pose;
-  uint32_t                 hCount;
-  double                   weight;
+	player_localize_hypoth_t hypothesis;
+	player_pose2d_t          pose;
+	uint32_t                 hCount;
+	double                   weight;
 
-  // Need some messing around to avoid a crash when the proxy is
-  // starting up.
+	// Need some messing around to avoid a crash when the proxy is
+	// starting up.
 
-  hCount = lp.GetHypothCount();
+	hCount = lp.GetHypothCount();
 
-  std::cout << "AMCL gives us " << hCount + 1 
+	std::cout << "AMCL gives us " << hCount + 1 
             << " possible locations:" << std::endl;
 
-  if(hCount > 0){
-    for(int i = 0; i <= hCount; i++){
-      hypothesis = lp.GetHypoth(i);
-      pose       = hypothesis.mean;
-      weight     = hypothesis.alpha;
-      std::cout << "X: " << pose.px << "\t";
-      std::cout << "Y: " << pose.py << "\t";
-      std::cout << "A: " << pose.pa << "\t";
-      std::cout << "W: " << weight  << std::endl;
+	if(hCount > 0){
+	    for(int i = 0; i <= hCount; i++){
+    		hypothesis = lp.GetHypoth(i);
+    		pose       = hypothesis.mean;
+    		weight     = hypothesis.alpha;
+    		std::cout << "X: " << pose.px << "\t";
+    		std::cout << "Y: " << pose.py << "\t";
+    		std::cout << "A: " << pose.pa << "\t";
+    		std::cout << "W: " << weight  << std::endl;
     }
-  }
+}
 
   // This just returns the mean of the last hypothesis, it isn't necessarily
   // the right one.
 
   return pose;
-} // End of readPosition()
+}
 
-void printLaserData(LaserProxy& sp)
-{
+//Take laser readins and print laser data
+void printLaserData(LaserProxy& sp) {
 
-  double maxRange, minLeft, minRight, range, bearing;
-  int points;
+double maxRange, minLeft, minRight, range, bearing;
+	int points;
 
-  maxRange  = sp.GetMaxRange();
-  minLeft   = sp.MinLeft();
-  minRight  = sp.MinRight();
-  range     = sp.GetRange(5);
-  bearing   = sp.GetBearing(5);
-  points    = sp.GetCount();
+	maxRange  = sp.GetMaxRange();
+	minLeft   = sp.MinLeft();
+	minRight  = sp.MinRight();
+	range     = sp.GetRange(5);
+	bearing   = sp.GetBearing(5);
+	points    = sp.GetCount();
 
-  //Print out useful laser data
-  std::cout << "Laser says..." << std::endl;
-  std::cout << "Maximum distance I can see: " << maxRange << std::endl;
-  std::cout << "Number of readings I return: " << points << std::endl;
-  std::cout << "Closest thing on left: " << minLeft << std::endl;
-  std::cout << "Closest thing on right: " << minRight << std::endl;
-  std::cout << "Range of a single point: " << range << std::endl;
-  std::cout << "Bearing of a single point: " << bearing << std::endl;
+	//Print out laser data
+	std::cout << "Laser says..." << std::endl;
+	std::cout << "Maximum distance I can see: " << maxRange << std::endl;
+	std::cout << "Number of readings I return: " << points << std::endl;
+	std::cout << "Closest thing on left: " << minLeft << std::endl;
+	std::cout << "Closest thing on right: " << minRight << std::endl;
+	std::cout << "Range of a single point: " << range << std::endl;
+	std::cout << "Bearing of a single point: " << bearing << std::endl;
 
-  return;
-} // End of printLaserData()
+	return;
+}
 
-/**
- *  printRobotData
- *
- * Print out data on the state of the bumpers and the current location
- * of the robot.
- *
- **/
-
-void printRobotData(BumperProxy& bp, player_pose2d_t pose)
-{
-
-  // Print out what the bumpers tell us:
-  std::cout << "Left  bumper: " << bp[0] << std::endl;
-  std::cout << "Right bumper: " << bp[1] << std::endl;
-  // Can also print the bumpers with:
-  //std::cout << bp << std::endl;
-
-  // Print out where we are
-  std::cout << "We are at" << std::endl;
-  std::cout << "X: " << pose.px << std::endl;
-  std::cout << "Y: " << pose.py << std::endl;
-  std::cout << "A: " << pose.pa << std::endl;
-
-  
-} // End of printRobotData()
+//Print bumpers and location
+void printRobotData(BumperProxy& bp, player_pose2d_t pose) {
+	// Print bumpers
+	std::cout << "Left  bumper: " << bp[0] << std::endl;
+	std::cout << "Right bumper: " << bp[1] << std::endl;
+	// Print location
+	std::cout << "We are at" << std::endl;
+	std::cout << "X: " << pose.px << std::endl;
+	std::cout << "Y: " << pose.py << std::endl;
+	std::cout << "A: " << pose.pa << std::endl; 
+}
