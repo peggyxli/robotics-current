0901748d5793d46c939a9ce82fce5f1ef056f389
Peggy Li <xpeggyxli@gmail.com>
2017-11-26 01:03:44 -0500

    Fix ending location; remove unused library

--- real-local.cc
+++ real-local.cc
@@ -7,7 +7,6 @@
  */
 
 #include <iostream>
-#include <cstdlib>
 #include <cmath>
 #include <fstream>
 #include <libplayerc++/playerc++.h>
@@ -51,7 +50,8 @@ int main(int argc, char *argv[]) {
 		outfile << "\nI am " << lp.GetHypoth(0).alpha*100 << " percent sure of my location." << std::endl;
 				  
 		double speed, turnrate, diffY, diffX, diffAngle;
-		while (pose.px < 5 || pose.py < -3.5) {	//navigate to position
+		while (pose.px < 4.9999 || pose.py < -3.5001) {	//navigate to position
+			//update and print information from the robot
 			robot.Read();
 			pose = readPosition(lp, outfile);
 			printRobotData(bp, pose, outfile);
@@ -66,12 +66,13 @@ int main(int argc, char *argv[]) {
 				speed = sp.MinRight()/2;
 			}
 			else {	//locate and move towards position
+				//calculate angle needed to move to end position
 				diffY = -3.5 - pose.py;
 				diffX = 5 - pose.px;
 				diffAngle = atan2(diffY, diffX) - pose.pa;
 				
-				turnrate = diffAngle;
-				if (diffAngle < 0.001)	//move to position
+				turnrate = diffAngle;	//set turnrate
+				if (diffAngle < 0.0001)	//move to position
 					speed = sqrt(diffY*diffY+diffX*diffX);
 				else speed = 0;	//stay in place and find angle
 			}
@@ -92,9 +93,8 @@ int main(int argc, char *argv[]) {
 /* Function provided by sample code
  * Read the position of the robot from the localization proxy. 
  *
- * The localization proxy gives us a set of "hypotheses", 
- * each of which is a number of possible locations for the robot,
- * and from each we extract the mean, which is a pose.
+ * The localization proxy gives us a set of "hypotheses", each of which is a number of 
+ * possible locations for the robot, and from each we extract the mean, which is a pose.
  */
 player_pose2d_t readPosition(LocalizeProxy& lp, std::ofstream& outfile) {
 	player_pose2d_t pose;
@@ -117,8 +117,9 @@ player_pose2d_t readPosition(LocalizeProxy& lp, std::ofstream& outfile) {
 	return pose;
 }
 
-//Function provided by sample code
-//Take laser readings and print laser data
+/* Function provided by sample code
+ * Take laser readings and print laser data
+ */
 void printLaserData(LaserProxy& sp, std::ofstream& outfile) {
 	//Print laser data
 	outfile << "Laser says..." << std::endl;
@@ -129,8 +130,9 @@ void printLaserData(LaserProxy& sp, std::ofstream& outfile) {
 	return;
 }
 
-//Function provided by sample code
-//Print bumpers and location
+/* Function provided by sample code
+ * Print bumpers and location
+ */
 void printRobotData(BumperProxy& bp, player_pose2d_t pose, std::ofstream& outfile) {
 	// Print bumpers
 	outfile << "Left  bumper: " << bp[0] << std::endl;
