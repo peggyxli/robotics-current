a17c2baa0491a1b067f1ade8a11e923cce4d1dd0
Peggy Li <xpeggyxli@gmail.com>
2017-11-26 00:21:29 -0500

    Change output to outfile

--- real-local.cc
+++ real-local.cc
@@ -9,14 +9,14 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#include <fstream>
 #include <libplayerc++/playerc++.h>
 using namespace PlayerCc;  
 
 //Function headers
-player_pose2d_t readPosition(LocalizeProxy& lp);
-void printLaserData(LaserProxy& sp);
-void printRobotData(BumperProxy& bp, player_pose2d_t pose);
-void chooseRandom(std::string, double&);
+player_pose2d_t readPosition(LocalizeProxy& lp, std::ofstream&);
+void printLaserData(LaserProxy& sp, std::ofstream&);
+void printRobotData(BumperProxy& bp, player_pose2d_t pose, std::ofstream&);
 
 int main(int argc, char *argv[]) {  
 	//set up proxies to connect the interface to the robot
@@ -29,28 +29,31 @@ int main(int argc, char *argv[]) {
 	LaserProxy sp (&robot, 0);
 	
 	bool locationFound = false;
+	std::ofstream outfile;
+	outfile.open("log.txt");
+	
 	pp.SetMotorEnable(true);
 	pp.SetSpeed(0.2, 0.1);
 
 	while (!locationFound) {	
     	robot.Read();
-    	pose = readPosition(lp);
+    	pose = readPosition(lp, outfile);
 		if (lp.GetHypothCount() > 0)
 			if (lp.GetHypoth(0).alpha > .9 && lp.GetHypothCount() < 3)
 				locationFound = true;	
     }
 
 	if (pose.px < -4 && pose.py < -4) {
-		std::cout << "\nSuccess!\nI have successfully located my position.";
-		std::cout << "\nI am at (" << pose.px << "," << pose.py << ").";
-		std::cout << "\nI am " << lp.GetHypoth(0).alpha*100 << " percent sure of my location." << std::endl;
+		outfile << "\nSuccess!\nI have successfully located my position.";
+		outfile << "\nI am at (" << pose.px << "," << pose.py << ").";
+		outfile << "\nI am " << lp.GetHypoth(0).alpha*100 << " percent sure of my location." << std::endl;
 				  
 		double speed, turnrate, diffY, diffX, diffAngle;
 		while (pose.px < 5 || pose.py < -3.5) {
 			robot.Read();
-			pose = readPosition(lp);
-			printRobotData(bp, pose);
-			printLaserData(sp);
+			pose = readPosition(lp, outfile);
+			printRobotData(bp, pose, outfile);
+			printLaserData(sp, outfile);
 			
 			if (sp.MinLeft() < .5) {
 				turnrate = sp.MinLeft() - 2;
@@ -71,13 +74,15 @@ int main(int argc, char *argv[]) {
 				else speed = 0;
 			}
 
-			std::cout << "Speed: " << speed << std::endl;      
-			std::cout << "Turn rate: " << turnrate << std::endl << std::endl;
+			outfile << "Speed: " << speed << std::endl;      
+			outfile << "Turn rate: " << turnrate << std::endl << std::endl;
 			pp.SetSpeed(speed, turnrate);
 		}
-		std::cout << "\nSuccess!\nI have successfully navigated my final position.";
-		std::cout << "\nI am at (" << pose.px << "," << pose.py << ")." << std::endl;
+		outfile << "\nSuccess!\nI have successfully navigated my final position.";
+		outfile << "\nI am at (" << pose.px << "," << pose.py << ")." << std::endl;
 	}
+	outfile.close();
+	return 0;
 }
 
 
@@ -88,21 +93,21 @@ int main(int argc, char *argv[]) {
  * each of which is a number of possible locations for the robot,
  * and from each we extract the mean, which is a pose.
  */
-player_pose2d_t readPosition(LocalizeProxy& lp) {
+player_pose2d_t readPosition(LocalizeProxy& lp, std::ofstream& outfile) {
 	player_pose2d_t pose;
 	double weight;
 	uint32_t hCount = lp.GetHypothCount();
 	
 	//Print AMCL data
-	std::cout << "\nAMCL gives us " << hCount + 1 << " possible locations:" << std::endl;
+	outfile << "\nAMCL gives us " << hCount + 1 << " possible locations:" << std::endl;
 	if(hCount > 0){	//check first, to avoid seg fault when booting up
 	    for(int i = 0; i <= hCount; i++){
     		pose       = lp.GetHypoth(i).mean;
     		weight     = lp.GetHypoth(i).alpha;
-    		std::cout << "X: " << pose.px << "\t";
-    		std::cout << "Y: " << pose.py << "\t";
-    		std::cout << "A: " << pose.pa << "\t";
-    		std::cout << "W: " << weight  << std::endl;
+    		outfile << "X: " << pose.px << "\t";
+    		outfile << "Y: " << pose.py << "\t";
+    		outfile << "A: " << pose.pa << "\t";
+    		outfile << "W: " << weight  << std::endl;
 		}
 		return lp.GetHypoth(0).mean;	//return most likely hypothesis
 	}
@@ -111,26 +116,26 @@ player_pose2d_t readPosition(LocalizeProxy& lp) {
 
 
 //Take laser readings and print laser data
-void printLaserData(LaserProxy& sp) {
+void printLaserData(LaserProxy& sp, std::ofstream& outfile) {
 	//Print laser data
-	std::cout << "Laser says..." << std::endl;
-	std::cout << "Maximum distance I can see: " << sp.GetMaxRange() << std::endl;
-	std::cout << "Number of readings I return: " << sp.GetCount() << std::endl;
-	std::cout << "Closest thing on left: " << sp.MinLeft() << std::endl;
-	std::cout << "Closest thing on right: " << sp.MinRight() << std::endl;
+	outfile << "Laser says..." << std::endl;
+	outfile << "Maximum distance I can see: " << sp.GetMaxRange() << std::endl;
+	outfile << "Number of readings I return: " << sp.GetCount() << std::endl;
+	outfile << "Closest thing on left: " << sp.MinLeft() << std::endl;
+	outfile << "Closest thing on right: " << sp.MinRight() << std::endl;
 	return;
 }
 
 
 //Print bumpers and location
-void printRobotData(BumperProxy& bp, player_pose2d_t pose) {
+void printRobotData(BumperProxy& bp, player_pose2d_t pose, std::ofstream& outfile) {
 	// Print bumpers
-	std::cout << "Left  bumper: " << bp[0] << std::endl;
-	std::cout << "Right bumper: " << bp[1] << std::endl;
+	outfile << "Left  bumper: " << bp[0] << std::endl;
+	outfile << "Right bumper: " << bp[1] << std::endl;
 	// Print location
-	std::cout << "We are at" << std::endl;
-	std::cout << "X: " << pose.px << std::endl;
-	std::cout << "Y: " << pose.py << std::endl;
-	std::cout << "A: " << pose.pa << std::endl;
+	outfile << "We are at" << std::endl;
+	outfile << "X: " << pose.px << std::endl;
+	outfile << "Y: " << pose.py << std::endl;
+	outfile << "A: " << pose.pa << std::endl;
 	return;
 }
